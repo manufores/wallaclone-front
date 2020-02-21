b53d9431f6ee131b60d6f805ded4fd1f6c48f4ec
Manuel Fores <manu_fores@yahoo.es>
2020-02-21 17:58:58 +0100

    wallaclone-front

---
+++ precache-manifest.59d82196b51fcfc84dc62a81bfa59633.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e78f2a7b4b3ea0887a1f5d2292d115",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2006e02bfbdedf9ea207",
+    "url": "/static/css/main.753c3ae7.chunk.css"
+  },
+  {
+    "revision": "1d2eeb51c2f83f89d043",
+    "url": "/static/js/2.41038f99.chunk.js"
+  },
+  {
+    "revision": "2006e02bfbdedf9ea207",
+    "url": "/static/js/main.825781ce.chunk.js"
+  },
+  {
+    "revision": "6781c3e3999aff840f62",
+    "url": "/static/js/runtime-main.f76319ef.js"
+  }
+]);
